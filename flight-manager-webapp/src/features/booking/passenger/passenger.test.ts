729463be2736eae0b8a5d11a0b84195fcb786456
Caio Calisto <caiocesarcalisto@gmail.com>
2024-06-14 19:04:59 +0100

    create model with properties and behaviours. Added unit tests

---
+++ flight-manager-webapp/src/features/booking/passenger/passenger.test.ts
@@ -0,0 +1,50 @@
+import {Passenger} from "./passenger";
+
+export * from '@testing-library/react'
+
+it('should map passenger with all data', () => {
+    const title = 'MR';
+    const gender = 'MALE';
+    const firstName = 'Caio';
+    const lastName = 'Calisto';
+    const dateOfBirth = '1988-08-20';
+    const passenger = new Passenger(title, gender, firstName, lastName, dateOfBirth)
+
+    expect(passenger.getTitle()).toBe(title)
+    expect(passenger.getGender()).toBe(gender)
+    expect(passenger.getFirstName()).toBe(firstName)
+    expect(passenger.getLastName()).toBe(lastName)
+    expect(passenger.getDateOfBirth()).toBe(dateOfBirth)
+})
+
+it('should change firstName 1 character without error', () => {
+    const passenger = new Passenger('MR', 'MALE', 'Natalis', 'Calisto', '1988-08-20')
+    const newFirstName = 'Natalia';
+    passenger.setFirstName(newFirstName)
+
+    expect(passenger.getFirstName()).toBe(newFirstName)
+})
+
+it('should change firstName 2 character without error', () => {
+    const passenger = new Passenger('MR', 'MALE', 'Nstalis', 'Calisto', '1988-08-20')
+    const newFirstName = 'Natalia';
+    passenger.setFirstName(newFirstName)
+
+    expect(passenger.getFirstName()).toBe(newFirstName)
+})
+
+it('should change firstName 3 character without error', () => {
+    const passenger = new Passenger('MR', 'MALE', 'Nstslis', 'Calisto', '1988-08-20')
+    const newFirstName = 'Natalia';
+    passenger.setFirstName(newFirstName)
+
+    expect(passenger.getFirstName()).toBe(newFirstName)
+})
+
+it('should add error if firstName change more than 3 characters', () => {
+    const passenger = new Passenger('MR', 'MALE', 'Nstsluq', 'Calisto', '1988-08-20')
+    passenger.setFirstName('Natalia')
+
+    expect(passenger.getErrors().length).toBe(1)
+    expect(passenger.getErrors()[0]).toBe('Maximum a change of 3 digits for first name is allowed')
+})
---
+++ flight-manager-webapp/src/features/booking/passenger/passenger.ts
@@ -0,0 +1,58 @@
+export class Passenger {
+    private title: string;
+    private gender: string;
+    private firstName: string;
+    private lastName: string;
+    private dateOfBirth: string;
+    private errors: string[];
+
+    constructor(title: string, gender: string, firstName: string, lastName: string, dateOfBirth: string) {
+        this.title = title;
+        this.gender = gender;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.errors = [];
+    }
+
+    getTitle(): string {
+        return this.title
+    }
+
+    getGender(): string {
+        return this.gender;
+    }
+
+    getFirstName(): string {
+        return this.firstName;
+    }
+
+    getLastName(): string {
+        return this.lastName;
+    }
+
+    getDateOfBirth(): string {
+        return this.dateOfBirth;
+    }
+
+    getErrors(): string[] {
+        return this.errors;
+    }
+
+    setFirstName(newFirstName: string): void {
+        const minLength = Math.min(newFirstName.length, this.firstName.length);
+        let differenceCount = 0;
+
+        for (let i = 0; i < minLength; i++) {
+            if (newFirstName[i] !== this.firstName[i]) {
+                differenceCount++;
+            }
+        }
+
+        if (differenceCount > 3) {
+            this.errors.push('Maximum a change of 3 digits for first name is allowed')
+        } else {
+            this.firstName = newFirstName;
+        }
+    }
+}
