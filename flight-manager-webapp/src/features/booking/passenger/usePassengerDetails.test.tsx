2409f8d69ab8863cb28aa13d8fddf011bcbef259
Caio Calisto <caiocesarcalisto@gmail.com>
2024-06-14 22:46:10 +0100

    unnecessary code

--- flight-manager-webapp/src/features/booking/passenger/usePassengerDetails.test.tsx
+++ flight-manager-webapp/src/features/booking/passenger/usePassengerDetails.test.tsx
@@ -2,7 +2,6 @@ import useData from "../../../hooks/useData";
 import {Passenger} from "./passenger";
 import {usePassengerDetails} from "./usePassengerDetails";
 import {renderHook} from "@testing-library/react";
-import {act} from "react";
 
 jest.mock('../../../hooks/useData')
 
@@ -13,9 +12,7 @@ it('should return passengers', () => {
     (useData as jest.Mock).mockImplementation(() => ({
         data: passengers
     }))
-    const { result } = renderHook(() => usePassengerDetails(['1', '2']))
+    const {result} = renderHook(() => usePassengerDetails(['1', '2']))
 
-    act(() => {
-        expect(result.current.data).toBe(passengers)
-    })
+    expect(result.current.data).toBe(passengers)
 })
