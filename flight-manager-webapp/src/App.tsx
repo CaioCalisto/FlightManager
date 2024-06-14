cd0afcf1db7a59e0b5f357912e8105d45480929b
Caio Calisto <caiocesarcalisto@gmail.com>
2024-06-14 09:49:31 +0100

    installed MaterialUI

--- flight-manager-webapp/src/App.tsx
+++ flight-manager-webapp/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Button, TextField} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
         Learn React
+        <Button variant="outlined">Example</Button>
+        <TextField id="standard-basic" label="Example" variant="standard" />
     </div>
   );
 }
